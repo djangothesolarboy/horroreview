bc104453eaaf152526e7c76f0f23baddaf2aff57
djangothesolarboy <warwickjunglex@gmail.com>
2021-10-19 16:56:56 -0400

    Added AHS review pages

--- reviews/button-display.js
+++ reviews/button-display.js
@@ -2,7 +2,19 @@ function display() {
     const element = document.querySelector('.display-episode-review');
     const button = document.querySelector('.display-button');
 
-    // element.style.display = 'none';
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
+    }
+}
+
+function display2() {
+    const element = document.querySelector('.display-episode-review2');
+    const button = document.querySelector('.display-button2');
+
     if (element.style.display === 'none') {
         element.style.display = 'block';
         button.style.transform = 'rotate(90deg)';
