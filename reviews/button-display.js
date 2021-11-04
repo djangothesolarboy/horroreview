29c6a705349982a642cd401559e2248f930e9f77
Damien Darko <warwickjunglex@gmail.com>
2021-11-04 16:48:26 -0400

    Chucky Ep Update

--- reviews/button-display.js
+++ reviews/button-display.js
@@ -23,3 +23,22 @@ function display2() {
         button.style.transform = 'rotate(360deg)';
     }
 }
+
+function display3() {
+    const element = document.querySelector('.display-episode-review3');
+    const button = document.querySelector('.display-button3');
+
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
+    }
+}
+
+// class Episode {
+//     constructor() {
+
+//     }
+// }
