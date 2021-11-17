3b32a02f05a61da049bdb78ef5ea5db497997aa4
Damien Darko <warwickjunglex@gmail.com>
2021-11-17 16:07:26 -0500

    Fixed buttons🎃

--- reviews/button-display.js
+++ reviews/button-display.js
@@ -1,62 +1,39 @@
-// function display() {
-//     const element = document.querySelector('.display-episode-review');
-//     const button = document.querySelector('.display-button');
+function display() {
+    const element = document.querySelector('.display-episode-review');
+    const button = document.querySelector('.display-button');
 
-//     if (element.style.display === 'none') {
-//         element.style.display = 'block';
-//         button.style.transform = 'rotate(90deg)';
-//     } else {
-//         element.style.display = 'none';
-//         button.style.transform = 'rotate(360deg)';
-//     }
-// }
-
-// function display2() {
-//     const element = document.querySelector('.display-episode-review2');
-//     const button = document.querySelector('.display-button2');
-
-//     if (element.style.display === 'none') {
-//         element.style.display = 'block';
-//         button.style.transform = 'rotate(90deg)';
-//     } else {
-//         element.style.display = 'none';
-//         button.style.transform = 'rotate(360deg)';
-//     }
-// }
-
-// function display3() {
-//     const element = document.querySelector('.display-episode-review3');
-//     const button = document.querySelector('.display-button3');
-
-//     if (element.style.display === 'none') {
-//         element.style.display = 'block';
-//         button.style.transform = 'rotate(90deg)';
-//     } else {
-//         element.style.display = 'none';
-//         button.style.transform = 'rotate(360deg)';
-//     }
-// }
-
-class Episode {
-    constructor(ep) {
-        this.ep = ep;
-    }
-    
-    toggleEpisode() {
-        let element = document.querySelector(`.display-episode-review${this.ep}`);
-        let button = document.querySelector(`.display-button`);
-        console.log(element, button)
-        if (element.style.display === 'none') {
-            element.style.display = 'block';
-            button.style.transform = 'rotate(90deg)';
-        } else {
-            element.style.display = 'none';
-            button.style.transform = 'rotate(360deg)';
-        }
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
     }
 }
 
-let ep1_1 = new Episode('1-1');
-let ep1_2 = new Episode('1-2');
-let ep1_3 = new Episode('1-3');
-console.log(ep1_2)
+function display2() {
+    const element = document.querySelector('.display-episode-review2');
+    const button = document.querySelector('.display-button2');
+
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
+    }
+}
+
+function display3() {
+    const element = document.querySelector('.display-episode-review3');
+    const button = document.querySelector('.display-button3');
+
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
+    }
+}
+
