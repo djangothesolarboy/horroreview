fa98d76a7450f56a682cc26080515b3ac80c9f3f
Damien Darko <warwickjunglex@gmail.com>
2021-11-18 00:44:33 -0500

    Chucky Episode 4 added; gitignore added🎃

--- reviews/button-display.js
+++ reviews/button-display.js
@@ -37,3 +37,15 @@ function display3() {
     }
 }
 
+function display4() {
+    const element = document.querySelector('.display-episode-review4');
+    const button = document.querySelector('.display-button4');
+
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+        button.style.transform = 'rotate(90deg)';
+    } else {
+        element.style.display = 'none';
+        button.style.transform = 'rotate(360deg)';
+    }
+}
